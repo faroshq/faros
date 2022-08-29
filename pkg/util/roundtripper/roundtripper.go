8a9c7af79024b2f603bf405e1aa579e2d2dcc302
Mangirdas Judeikis <mangirdas@judeikis.lt>
2022-08-29 20:20:06 +0300

    typo

--- pkg/util/roundtripper/roundtripper.go
+++ pkg/util/roundtripper/roundtripper.go
@@ -6,8 +6,8 @@ import (
 
 type RoundTripperFunc func(*http.Request) (*http.Response, error)
 
-// RoundTripNoop is noop roundtripper to be used for test or as a placeholder for
+// RoundTripperFunc is noop roundtripper to be used for test or as a placeholder for
 // other integrations
-func (f RoundTripperFunc) RoundTripNoop(req *http.Request) (*http.Response, error) {
+func (f RoundTripperFunc) RoundTripperFunc(req *http.Request) (*http.Response, error) {
 	return f(req)
 }
