bdba0f0659a02a437e7535f78b97356575a81487
Mangirdas Judeikis <mangirdas@judeikis.lt>
2022-09-06 17:48:27 +0300

    fix https://github.com/golang/go/issues/23643

--- pkg/service/middleware/panic.go
+++ pkg/service/middleware/panic.go
@@ -12,6 +12,11 @@ func Panic(log *logrus.Entry) func(http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			defer func() {
 				if e := recover(); e != nil {
+					// https://github.com/golang/go/issues/23643
+					// This panics on streaming so we catch and ignore :/
+					if e == http.ErrAbortHandler {
+						return // ignore
+					}
 					log.Errorf("panic: %#v\n%s\n", e, string(debug.Stack()))
 					http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 				}
