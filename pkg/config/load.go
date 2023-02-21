60a510374c3ff4b56f0715881372e1d7b5f8c180
Mangirdas Judeikis <mangirdas@judeikis.lt>
2023-02-21 19:14:57 +0200

    kubeconfig loading rules

--- pkg/config/load.go
+++ pkg/config/load.go
@@ -2,15 +2,15 @@ package config
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 	"github.com/kelseyhightower/envconfig"
 
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 
-	utilkubernetes "github.com/faroshq/faros/pkg/util/kubernetes"
+	utilfile "github.com/faroshq/faros/pkg/util/file"
 )
 
 // LoadController loads the configuration from the environment and flags
@@ -31,39 +31,23 @@ func Load() (*Config, error) {
 		c.APIConfig.OIDCAuthSessionKey = uuid.Must(uuid.NewUUID()).String()
 	}
 
-	hostingKubeConfig, err := loadKubeConfig(c.FarosKCPConfig.HostingClusterKubeConfigPath)
+	rest, err := loadKubeConfig(c.FarosKCPConfig.HostingClusterKubeConfigPath)
 	if err != nil {
-		return nil, fmt.Errorf("failed to load hosting cluster kubeconfig: %w", err)
+		return nil, err
 	}
+	c.FarosKCPConfig.HostingClusterRestConfig = rest
 
-	c.FarosKCPConfig.HostingClusterRestConfig, err = hostingKubeConfig.ClientConfig()
+	rest, err = loadKubeConfig(c.FarosKCPConfig.KCPClusterKubeConfigPath)
 	if err != nil {
-		return nil, fmt.Errorf("failed to load hosting cluster rest config: %w", err)
+		return nil, err
 	}
+	c.FarosKCPConfig.HostingClusterRestConfig = rest
 
 	kcpKubeConfig, err := loadKubeConfig(c.FarosKCPConfig.KCPClusterKubeConfigPath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to load kcp cluster kubeconfig: %w", err)
 	}
-
-	kcpRest, err := kcpKubeConfig.ClientConfig()
-	if err != nil {
-		return nil, fmt.Errorf("failed to load kcp cluster rest config: %w", err)
-	}
-
-	// Sanitize as we don't know what user did in the kubeconfig
-	// TODO: Clean duplicate code
-	cf, err := utilkubernetes.NewClientFactory(kcpRest)
-	if err != nil {
-		return nil, err
-	}
-
-	rest, err := cf.GetRootRestConfig()
-	if err != nil {
-		return nil, err
-	}
-
-	c.FarosKCPConfig.KCPClusterRestConfig = rest
+	c.FarosKCPConfig.KCPClusterRestConfig = kcpKubeConfig
 
 	return c, err
 }
@@ -72,19 +56,24 @@ func Load() (*Config, error) {
 // intended to be common between fixture for servers whose lifecycle
 // is test-managed and fixture for servers whose lifecycle is managed
 // separately from a test run.
-func loadKubeConfig(kubeconfigPath string) (clientcmd.ClientConfig, error) {
-	fs, err := os.Stat(kubeconfigPath)
+func loadKubeConfig(kubeconfigPath string) (*rest.Config, error) {
+	exists, err := utilfile.Exist(kubeconfigPath)
 	if err != nil {
 		return nil, err
 	}
-	if fs.Size() == 0 {
-		return nil, fmt.Errorf("%s points to an empty file", kubeconfigPath)
+	if !exists {
+		config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+		if err != nil {
+			return nil, err
+		}
+		return config, nil
+	} else {
+		rawConfig, err := clientcmd.LoadFromFile(kubeconfigPath)
+		if err != nil {
+			return nil, fmt.Errorf("failed to load admin kubeconfig: %w", err)
+		}
+
+		return clientcmd.NewNonInteractiveClientConfig(*rawConfig, rawConfig.CurrentContext, nil, nil).ClientConfig()
 	}
 
-	rawConfig, err := clientcmd.LoadFromFile(kubeconfigPath)
-	if err != nil {
-		return nil, fmt.Errorf("failed to load admin kubeconfig: %w", err)
-	}
-
-	return clientcmd.NewNonInteractiveClientConfig(*rawConfig, rawConfig.CurrentContext, nil, nil), nil
 }
